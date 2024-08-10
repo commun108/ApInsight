fb0e31cebaba8299f96f44d34c60bcc0b67dfb73
Jakob G. <commun108@gmail.com>
2024-08-10 07:41:41 +0200

    Added a ComboBox (drop-down) to hold the different colormaps

--- GUI/DepthsliceViewer_main.py
+++ GUI/DepthsliceViewer_main.py
@@ -1,22 +1,23 @@
 """GUI libraries"""
-import tkinter as tk
-from tkinter import Frame, Button, Menu, filedialog, messagebox, PanedWindow, PhotoImage, Toplevel, Text, END
-import platform
-import webbrowser
-
-from screeninfo import get_monitors
-import json
-
-"""imports from own scripts:"""
-from GUI.EditProjectFile import start_projectGUI
-from GUI.ImageFrame import ImageFrame
+from GUI.RightFrame import RightFrame
 from GUI.MeasurementTool import MeasurementWindow
 from GUI.LeftFrame import LeftFrame
-from GUI.RightFrame import RightFrame
-
+from GUI.ImageFrame import ImageFrame
+from GUI.error_handling import show_error_dialog
+from GUI.EditProjectFile import start_projectGUI
 from data.ProjectData import ProjectData
 from config_manager import ConfigurationManager
-from GUI.error_handling import show_error_dialog
+import json
+import platform
+import tkinter as tk
+import webbrowser
+from tkinter import (END, Button, Frame, Menu, PanedWindow, PhotoImage, Text,
+                     Toplevel, filedialog, messagebox)
+
+from screeninfo import get_monitors
+
+"""imports from own scripts:"""
+
 
 monitors = get_monitors()
 screen_res_primary = [monitors[0].height, monitors[0].width]
@@ -57,10 +58,12 @@ class GuiLayout(object):
                                       width=width - width_frame_left - width_frame_right,
                                       height=height_frame)
 
-        self.top_frame = TopFrame(self.master, self.frame_image, width=width, height=height_frame // 20)
+        self.top_frame = TopFrame(
+            self.master, self.frame_image, width=width, height=height_frame // 20)
         self.top_frame.pack(side='top', fill='x', padx=5, pady=5)
 
-        self.frame_left = LeftFrame(self.pwindow, self.frame_image, width=width_frame_left, height=height_frame)
+        self.frame_left = LeftFrame(
+            self.pwindow, self.frame_image, width=width_frame_left, height=height_frame)
         self.frame_right = RightFrame(self.pwindow, self.frame_image, self.frame_left, self.menu_builder,
                                       self.project_data,
                                       width=width_frame_right, height=height_frame)
@@ -113,11 +116,13 @@ class GuiLayout(object):
             # Calculate the new widths for the frames
             new_width_frame_left = current_width // 8
             new_width_frame_image = int(current_width // 1.5)
-            new_width_frame_right = current_width - new_width_frame_left - new_width_frame_image
+            new_width_frame_right = current_width - \
+                new_width_frame_left - new_width_frame_image
 
             # Adjust the sashes of the PanedWindow
             self.pwindow.sash_place(0, new_width_frame_left, 0)
-            self.pwindow.sash_place(1, new_width_frame_left + new_width_frame_image, 0)
+            self.pwindow.sash_place(
+                1, new_width_frame_left + new_width_frame_image, 0)
 
             # Update the last window width
             self.last_window_width = current_width
@@ -150,7 +155,8 @@ class MenuBuilder:
         filemenu.add_separator()
         filemenu.add_command(label="Open project", command=self.open_project)
         filemenu.add_command(label="Save project", command=self.save_project)
-        filemenu.add_command(label="Save project as", command=self.save_project_as)
+        filemenu.add_command(label="Save project as",
+                             command=self.save_project_as)
         filemenu.add_separator()
         filemenu.add_command(label="Options", command=self.open_options_window)
         filemenu.add_separator()
@@ -181,7 +187,8 @@ class MenuBuilder:
         window = Toplevel(self.master)
         window.title(title)
 
-        txt = Text(window, wrap="word", width=60, height=18, background="light gray")
+        txt = Text(window, wrap="word", width=60,
+                   height=18, background="light gray")
         txt.pack(padx=10, pady=10)
         txt.insert(END, text)
         txt.config(state="disabled")
@@ -192,7 +199,8 @@ class MenuBuilder:
                 webbrowser.open_new(link)
 
             txt.tag_config(tag_name, foreground="blue", underline=1)
-            txt.tag_bind(tag_name, "<Enter>", lambda e: txt.config(cursor="hand2"))
+            txt.tag_bind(tag_name, "<Enter>",
+                         lambda e: txt.config(cursor="hand2"))
             txt.tag_bind(tag_name, "<Leave>", lambda e: txt.config(cursor=""))
             txt.tag_bind(tag_name, "<Button-1>", open_link)
             txt.tag_add(tag_name, start_index, end_index)
@@ -200,7 +208,8 @@ class MenuBuilder:
         # Adding hyperlinks
         add_hyperlink("hyper1", "https://youtu.be/f0AKAc0pgbo", "4.0", "4.end")
         add_hyperlink("hyper2", "https://youtu.be/lIJPaZ917v4", "7.0", "7.end")
-        add_hyperlink("hyper3", "https://github.com/erichnau/ApInsight.git", "10.0", "10.end")
+        add_hyperlink(
+            "hyper3", "https://github.com/erichnau/ApInsight.git", "10.0", "10.end")
         add_hyperlink("hyper4", "mailto:erich.nau@niku.no", "13.0", "13.end")
 
         # Close button
@@ -210,7 +219,8 @@ class MenuBuilder:
         window.geometry("500x350")  # Adjust the size as needed
 
     def open_project(self):
-        self.json_file = filedialog.askopenfilename(filetypes=[("JSON Files", "*.json")])
+        self.json_file = filedialog.askopenfilename(
+            filetypes=[("JSON Files", "*.json")])
         if self.json_file:
             # Clear previous project data and related elements
             if self.project_data:
@@ -255,12 +265,14 @@ class MenuBuilder:
 
             if file_path:
                 # Load the current project data
-                project_data = self.project_data.load_project_from_json(self.json_file)
+                project_data = self.project_data.load_project_from_json(
+                    self.json_file)
 
                 # Serialize the sections data
                 sections_data = {}
                 if len(self.frame_right.sections) > 0:
-                    sections_data = self.serialize_sections(self.frame_right.sections)
+                    sections_data = self.serialize_sections(
+                        self.frame_right.sections)
 
                 # Organize the data with 'projects' and 'sections' keys
                 data = {
@@ -282,12 +294,14 @@ class MenuBuilder:
         # Check if the current project file path is set
         if self.json_file:
             # Load the current project data
-            project_data = self.project_data.load_project_from_json(self.json_file)
+            project_data = self.project_data.load_project_from_json(
+                self.json_file)
 
             # Serialize the sections data
             sections_data = {}
             if len(self.frame_right.sections) > 0:
-                sections_data = self.serialize_sections(self.frame_right.sections)
+                sections_data = self.serialize_sections(
+                    self.frame_right.sections)
 
             # Organize the data with 'projects' and 'sections' keys
             data = {
@@ -305,7 +319,8 @@ class MenuBuilder:
         serialized_data = {}
         for name, info in sections.items():
             serialized_data[name] = {
-                'select': info['select'].get(),  # Assuming this is a BooleanVar
+                # Assuming this is a BooleanVar
+                'select': info['select'].get(),
                 'keep': info['keep'].get(),  # Assuming this is a BooleanVar
                 'start': info['start'],  # Assuming this is a list or tuple
                 'end': info['end']  # Assuming this is a list or tuple
@@ -325,11 +340,13 @@ class MenuBuilder:
                         "\nopen-source Python-program (which is significantly slower converting .fld files)"
             info_label = tk.Label(info_window, text=info_text)
             info_label.pack()
-            info_button = tk.Button(info_window, text="Close", command=info_window.destroy)
+            info_button = tk.Button(
+                info_window, text="Close", command=info_window.destroy)
             info_button.pack()
 
         self.config_manager = ConfigurationManager('config.ini')
-        self.use_compiled_exe = self.config_manager.get_boolean_option('Application', 'use_compiled_exe')
+        self.use_compiled_exe = self.config_manager.get_boolean_option(
+            'Application', 'use_compiled_exe')
         self.vmin = int(self.config_manager.get_option('Greyscale', 'vmin'))
         self.vmax = int(self.config_manager.get_option('Greyscale', 'vmax'))
 
@@ -340,23 +357,28 @@ class MenuBuilder:
         else:
             icon_image = PhotoImage(file='icon2.png')
             options_window.iconphoto(True, icon_image)
-        options_window.geometry('300x250')  # Adjusted size to accommodate new frame
+        # Adjusted size to accommodate new frame
+        options_window.geometry('300x250')
 
         # Use compiled exe
         label_exe = tk.Label(options_window, text="Use compiled exe:")
         label_exe.pack()
         use_compiled_exe_var = tk.BooleanVar(value=self.use_compiled_exe)
-        checkbutton_exe = tk.Checkbutton(options_window, variable=use_compiled_exe_var)
+        checkbutton_exe = tk.Checkbutton(
+            options_window, variable=use_compiled_exe_var)
         checkbutton_exe.pack()
 
-        info_button = tk.Button(options_window, text="Information", command=show_information)
+        info_button = tk.Button(
+            options_window, text="Information", command=show_information)
         info_button.pack()
 
         # Frame for default greyscale values
-        greyscale_frame = tk.Frame(options_window, borderwidth=2, relief="groove")
+        greyscale_frame = tk.Frame(
+            options_window, borderwidth=2, relief="groove")
         greyscale_frame.pack(pady=10)
 
-        label_greyscale = tk.Label(greyscale_frame, text="Default Greyscale Values for Section View")
+        label_greyscale = tk.Label(
+            greyscale_frame, text="Default Greyscale Values for Section View")
         label_greyscale.pack()
 
         # vmin option
@@ -380,7 +402,8 @@ class MenuBuilder:
             new_vmax = vmax_var.get()
 
             # Update the configuration file
-            self.config_manager.set_option('Application', 'use_compiled_exe', str(new_use_compiled_exe))
+            self.config_manager.set_option(
+                'Application', 'use_compiled_exe', str(new_use_compiled_exe))
             self.config_manager.set_option('Greyscale', 'vmin', new_vmin)
             self.config_manager.set_option('Greyscale', 'vmax', new_vmax)
             self.config_manager.save_config()
@@ -388,13 +411,15 @@ class MenuBuilder:
             options_window.destroy()
             messagebox.showinfo("Options", "Configuration updated.")
 
-        confirm_button = tk.Button(options_window, text="Confirm", command=confirm)
+        confirm_button = tk.Button(
+            options_window, text="Confirm", command=confirm)
         confirm_button.pack()
 
         def exit_options():
             options_window.destroy()
 
-        exit_button = tk.Button(options_window, text="Exit", command=exit_options)
+        exit_button = tk.Button(
+            options_window, text="Exit", command=exit_options)
         exit_button.pack()
 
 
@@ -414,14 +439,22 @@ class TopFrame(Frame):
         self.pack_propagate(0)
 
     def create_widgets(self):
-        self.zoom_button = Button(self, text="Zoom to Full Extent", command=self.zoom_to_full_extent, state='disabled')
+        self.zoom_button = Button(
+            self, text="Zoom to Full Extent", command=self.zoom_to_full_extent, state='disabled')
         self.zoom_button.pack(side="left", padx=5, pady=5)
 
-        self.draw_section_button = tk.Button(self, text="Draw Section", command=self.enable_draw_mode, state='disabled')
+        self.draw_section_button = tk.Button(
+            self, text="Draw Section", command=self.enable_draw_mode, state='disabled')
         self.draw_section_button.pack(side='left', padx=5, pady=5)
 
-        self.measure_tool = tk.Button(self, text="Measure Tool", command=self.measure, state='disabled')
+        self.measure_tool = tk.Button(
+            self, text="Measure Tool", command=self.measure, state='disabled')
         self.measure_tool.pack(side='left', padx=5, pady=5)
+
+        self.cmap_selector = tk.ComboBox(self,
+                                         state='readonly',
+                                         values=['Greys', 'jet', 'viridis'])
+        self.cmap_selector.pack(side='left', padx=5, pady=5)
 
     def zoom_to_full_extent(self):
         # Calculate the zoom level for the best fit
